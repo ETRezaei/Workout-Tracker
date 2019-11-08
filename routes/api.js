b2b0ff9a45b6a5935d42529194e9167259e52c7b
Emi Rezaei <tomoyose.emi@gmail.com>
2019-11-07 18:33:31 -0800

    created the post call

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,20 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+const mongoose = require("mongoose");
+const db = require("./models");
+
+
+app.post("/exercise", ({ body }, res) => {
+    db.Exercise.create(body)
+      .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
 
 
 module.exports = router;
